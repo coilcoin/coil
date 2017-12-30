3a418305a9ed85bc52664fc8f33dafdcfa2939bf
Jesse Sibley <jessesibley@protonmail.com>
2017-12-30 00:19:53 +0000

    Added some more routes

--- coil.py
+++ coil.py
@@ -5,6 +5,7 @@
 
 __version__ = "0.1.0"
 
+import json
 from aiohttp import web
 from wallet import Wallet
 from node import Node
@@ -23,7 +24,16 @@ async def index(request):
 async def chain(request):
 	return web.Response(text=node.getChain(), content_type="application/json")
 
+async def resolve(request):
+	return web.Response(text=node.getChain())
+
+async def block(request):
+	lastBlock = json.dumps(node.chain.lastBlock.__dict__)
+	return web.Response(text=lastBlock, content_type="application/json")
+
 app.router.add_get("/", index)
 app.router.add_get("/chain", chain)
+app.router.add_get("/chain/resolve", resolve)
+app.router.add_get("/block", block)
 
 web.run_app(app, host=node.host, port=node.port)
